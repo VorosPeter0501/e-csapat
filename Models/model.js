d78256f2d1e02a8c990f8a2a04e6bb13945b6e03
Szalai Mark <a.te@email.cimedszalai.mark@students.jedlik.eu>
2025-11-11 13:32:08 +0100

    Model updatelve

--- Models/model.js
+++ Models/model.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
- const dataSchema = new mongoose.Schema({
- name: {
- required: true,
- type: String
- },
- age: {
- required: true,
- type: Number
- }
- })
- module.exports = mongoose.model('Data', dataSchema)
+const markak_202511101237Schema = new mongoose.Schema({
+    marka_nev: {
+        required: true,
+        type: String
+    },
+    orszag: {
+        required: true,
+        type: String
+    },
+    alapitas_eve: {
+        required: true,
+        type: Number
+    }
+})
+module.exports = mongoose.model('markak_202511101237', markak_202511101237Schema)
--- routes/routes.js
+++ routes/routes.js
@@ -2,3 +2,31 @@ const express = require('express');
 const Model = require('../models/model');
 const router = express.Router();
 module.exports = router;
+
+
+router.patch('/:id', async (req, res) => {
+ try {
+ const id = req.params.id;
+ const updatedmarkak_202511101237 = req.body;
+ const options = { new: true };
+ const result = await Model.findByIdAndUpdate(
+ id, updatedmarkak_202511101237, options
+ )
+ res.send(result)
+ }
+ catch (error) {
+ res.status(400).json({ message: error.message })
+ }
+});
+
+
+router.delete('/:id', async (req, res) => {
+ try {
+ const id = req.params.id;
+ const markak_202511101237 = await Model.findByIdAndDelete(id)
+ res.send(`Document with ${markak_202511101237.name} has been deleted..`)
+ }
+ catch (error) {
+ res.status(400).json({ message: error.message })
+ }
+})
