ee1e958acce69614e4cd408fb0b991e24237100a
VorosPeter0501 <gladibox123@gmail.com>
2025-11-26 08:13:16 +0100

    megcsinálltam a controllert a queryhez

---
+++ controllers/markak.controller.js
@@ -0,0 +1,31 @@
+const Marka = require('../Models/model')
+
+exports.createMarka = async (req, res, next) => {
+    try {
+        const marka = await Marka.create(req.body);
+        res.status(201).json({ success: true, data: marka });
+    } catch (error) {
+        res.status(400).json({ success: false, msg: error.message });
+    }
+}
+
+exports.getAllMarka = async (req, res, next) => {
+    try {
+        const markas = await Marka.find();
+        res.status(200).json({ success: true, data: markas });
+    } catch (error) {
+        res.status(500).json({ success: false, msg: error.message });
+    }
+}
+
+exports.getMarka = async (req, res, next) => {
+    try {
+        const marka = await Marka.findById(req.params.id);
+        if (!marka) {
+            return res.status(400).json({ success: false, msg: 'Not found' });
+        }
+        res.status(200).json({ success: true, data: marka });
+    } catch (error) {
+        res.status(400).json({ success: false });
+    }
+};
